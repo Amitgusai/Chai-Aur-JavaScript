12c014271dc0f19369ee0c95b4d797bce8b5bc41
AmitGusai <amitgusai2003@gmail.com>
2024-03-02 00:12:39 +0530

    SCOPE

--- 03_basics/lecture_21.js
+++ 03_basics/lecture_21.js
@@ -1,8 +1,23 @@
-(".........................................................       SCOPE       ...........................................................");
+(".........................................................       SCOPE - 1      ...........................................................");
 
+// Global Scope:
+var c = 300
+let a = 100
+{}  - "When comes with object or function: then it is 'SCOPE'"
+if (true) {
+    // Block Scope:
+    let a = 10;
+    const b = 20;
+    // Problem: No Scope applied in var
+    var c = 30;
+    console.log("Inner", a)
+};
 
+console.log(a);
+// console.log(b);
+console.log(c);
 
-
+("For InterView: Scope in windows through Browser and in code enviroment through node are both Differenct");
 
 
 
