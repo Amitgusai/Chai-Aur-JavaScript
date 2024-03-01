fd812e586c0624de4b47900bba229646e1a38bf8
AmitGusai <amitgusai2003@gmail.com>
2024-03-01 22:51:34 +0530

    Update lecture_18.js

--- _basics_2/lecture_18.js
+++ _basics_2/lecture_18.js
@@ -1,7 +1,5 @@
 "..................................         Object De-Structure  and JSON       ................................................";
 
-// Concept used in React
-
 const login = {
   username: "amitgusai",
   password: 14848,
@@ -13,3 +11,45 @@ console.log(login.username);
 const { course_instructor: Instructor } = login;
 console.log(Instructor);
 
+// Concept used in React as: De-Structuring in React
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "qualcomm")
+
+"......................     JSON    ......................"
+// JSON
+// API values from BackEnd in JSON format
+
+// 1. Object form
+{
+  "name": "amit",
+  "course_name": "javascript",
+  "price": "im-measurable"
+}
+
+// 2. Array form
+[
+  {},
+  {},
+  {}
+]
+
+"..................   JSON Formatter  ................."
+
+// A JSON formatter is a tool that makes JSON data easier to read and understand.
+
+// Raw data: 
+{"name":"John","age":30,"cars":["Ford","BMW","Fiat"]}
+
+// Formatted JSON data:
+{
+  "name": "John",
+  "age": 30,
+  "cars": [
+      "Ford",
+      "BMW",
+      "Fiat"
+  ]
+}
